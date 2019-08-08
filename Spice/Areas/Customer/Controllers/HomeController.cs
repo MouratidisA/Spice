f2dd9302fccdd912393d61ac0e0047573e86ba57
Mouratidis Anargyros <argiris95@windowslive.com>
2019-08-08 15:18:44 +0300

    Customer and Admin Areas added

--- Spice/Areas/Customer/Controllers/HomeController.cs
+++ Spice/Areas/Customer/Controllers/HomeController.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Spice.Models;
+using System.Diagnostics;
 
-namespace Spice.Controllers
+namespace Spice.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         public IActionResult Index()
