71015c9b57c3682d25e7aab326738829ad3f53f8
MouratidisA <argiris95@windowslive.com>
2019-09-14 19:30:42 +0300

    Create and BackToList partial view

--- Spice/Areas/Admin/Controllers/CategoryController.cs
+++ Spice/Areas/Admin/Controllers/CategoryController.cs
@@ -21,5 +21,11 @@ namespace Spice.Areas.Admin.Controllers
         {
             return View( await _db.Category.ToListAsync());
         }
+
+        //GET - CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
