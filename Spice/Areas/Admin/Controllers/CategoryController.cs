9ad6830ea943a1e9065dd8374d65596f149f902b
MouratidisA <argiris95@windowslive.com>
2019-09-14 18:18:26 +0300

    Adding Category Controller and Get action

---
+++ Spice/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Spice.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Spice.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        //GET 
+        public async Task<IActionResult> Index()
+        {
+            return View( await _db.Category.ToListAsync());
+        }
+    }
+}
