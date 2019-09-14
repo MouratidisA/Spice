61a588d87a6d1d79a7ae86b51e702ab4526c6043
MouratidisA <argiris95@windowslive.com>
2019-09-14 20:36:25 +0300

    Delete action for Category added

--- Spice/Areas/Admin/Controllers/CategoryController.cs
+++ Spice/Areas/Admin/Controllers/CategoryController.cs
@@ -63,6 +63,7 @@ namespace Spice.Areas.Admin.Controllers
             return View(category);
         }
 
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(Category category)
@@ -76,6 +77,40 @@ namespace Spice.Areas.Admin.Controllers
             }
 
             return View(category);
+        }
+
+        //GET - DELETE
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _db.Category.FindAsync(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+
+
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int? id)
+        {
+            var category = await _db.Category.FindAsync(id);
+
+            if (category == null)
+                return View();
+
+            _db.Remove(category);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
