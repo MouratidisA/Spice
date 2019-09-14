0d14b2a2873797acf166d644fbc81743fa1b48dc
MouratidisA <argiris95@windowslive.com>
2019-09-14 19:41:38 +0300

    Fix Category Name change from int to string

--- Spice/Models/Category.cs
+++ Spice/Models/Category.cs
@@ -7,6 +7,6 @@ namespace Spice.Models
         [Key]
         public int Id  { get; set; }
         [Required]
-        public int Name  { get; set; }
+        public string Name  { get; set; }
     }
 }
