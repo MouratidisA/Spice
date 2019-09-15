6a11afc7de1b876393be30437de6ff3034773bbc
MouratidisA <argiris95@windowslive.com>
2019-09-15 23:09:48 +0300

    MenuItem added to Database and MenuItem controller created

--- Spice/Data/ApplicationDbContext.cs
+++ Spice/Data/ApplicationDbContext.cs
@@ -13,5 +13,6 @@ namespace Spice.Data
 
         public DbSet<Category> Category { set; get; }
         public DbSet<SubCategory> SubCategory { set; get; }
+        public DbSet<MenuItem> MenuItem { set; get; }
     }
 }
