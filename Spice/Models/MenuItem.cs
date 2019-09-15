32e67b02d7d9d4eed20b1624fe48d5ecb4dca3c1
MouratidisA <argiris95@windowslive.com>
2019-09-16 00:06:45 +0300

    MenuItem Model changes and Model View Created
    ModelView pending properties

--- Spice/Models/MenuItem.cs
+++ Spice/Models/MenuItem.cs
@@ -9,6 +9,7 @@ namespace Spice.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        public string Description { get; set; }
         public string Spicyness { get; set; }
 
         public enum ESpicy { NA = 0, NotSpicy = 1, Spicy = 2, VerySpicy = 3 }
