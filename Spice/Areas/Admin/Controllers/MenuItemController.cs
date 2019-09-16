a4aa3890c425ee1d7af0ba71ca03aeed1700df9b
MouratidisA <argiris95@windowslive.com>
2019-09-16 22:38:49 +0300

    MenutItem model fix, MenuItem View Model added, Get for Create added

--- Spice/Areas/Admin/Controllers/MenuItemController.cs
+++ Spice/Areas/Admin/Controllers/MenuItemController.cs
@@ -1,9 +1,10 @@
-﻿using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Spice.Data;
+using Spice.Models;
+using Spice.Models.ViewModels;
+using System.Threading.Tasks;
 
 namespace Spice.Areas.Admin.Controllers
 {
@@ -13,16 +14,31 @@ namespace Spice.Areas.Admin.Controllers
         private readonly ApplicationDbContext _db;
         private readonly IHostingEnvironment _hostingEnvironment;
 
+        [BindProperty]
+        public MenuItemViewModel MenuItemVM { get; set; }
+
         public MenuItemController(ApplicationDbContext db,IHostingEnvironment hostingEnvironment)
         {
             _db = db;
             _hostingEnvironment = hostingEnvironment;
+            MenuItemVM = new MenuItemViewModel()
+            {
+                Category = _db.Category,
+                MenuItem = new MenuItem()
+
+            };
         }
 
 
         public async Task<IActionResult> Index()
         {
             return View(await _db.MenuItem.Include(m=>m.Category).Include(m=>m.SubCategory).ToListAsync());
+        }
+
+        //GET - CREATE
+        public IActionResult Create()
+        {
+            return View(MenuItemVM);
         }
     }
 }
--- Spice/Models/ViewModels/MenuItemViewModel.cs
+++ Spice/Models/ViewModels/MenuItemViewModel.cs
@@ -1,11 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace Spice.Models.ViewModels
 {
     public class MenuItemViewModel
     {
+        public MenuItem MenuItem  { get; set; }
+        public IEnumerable<Category> Category { get; set; }
+        public IEnumerable<SubCategory> SubCategory { get; set; }
+
+
     }
 }
