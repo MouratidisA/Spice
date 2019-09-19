eeedb627265c8d63c81fde711f702f2097e0bc31
Mouratidis Anargyros <argiris95@windowslive.com>
2019-09-19 13:33:46 +0300

    Coupon Create view and Call Methods added

---
+++ Spice/Areas/Admin/Controllers/CouponController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spice.Data;
+using Spice.Models;
+
+namespace Spice.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CouponController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CouponController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        //GET
+        public async Task<IActionResult> Index()
+        {
+            return View(await _db.Coupon.ToListAsync());
+        }
+
+
+        //GET - CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST - CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Coupon coupon)
+        {
+            if (ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                if (files.Any())
+                {
+                    byte[] p1 = null;
+                    //Converting image into bytes and store it 
+                    using (var fs1 = files[0].OpenReadStream())
+                    {
+                        using (var ms1 = new MemoryStream())
+                        {
+                            fs1.CopyTo(ms1);
+                            p1 = ms1.ToArray();
+                        }
+                    }
+
+                    coupon.Picture = p1;
+                }
+
+                _db.Coupon.Add(coupon);
+                await _db.SaveChangesAsync();
+                
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(coupon);
+
+        }
+    }
+}
