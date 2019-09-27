3cf164e284934e5e0e319fd6ef9880e9f70b7794
Mouratidis Anargyros <argiris95@windowslive.com>
2019-09-27 10:48:49 +0300

    Add to Cart POST Action added

--- Spice/Areas/Customer/Controllers/HomeController.cs
+++ Spice/Areas/Customer/Controllers/HomeController.cs
@@ -3,8 +3,10 @@ using Spice.Models;
 using System.Diagnostics;
 using System.Linq;
 using System.Net;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Spice.Data;
 using Spice.Models.ViewModels;
@@ -18,7 +20,7 @@ namespace Spice.Areas.Customer.Controllers
 
         public HomeController(ApplicationDbContext db)
         {
-            _db = db;            
+            _db = db;
         }
 
 
@@ -26,13 +28,14 @@ namespace Spice.Areas.Customer.Controllers
         {
             IndexViewModel IndexVM = new IndexViewModel()
             {
-                MenuItems = await _db.MenuItem.Include(m=>m.Category).Include(m => m.SubCategory).ToListAsync(),
+                MenuItems = await _db.MenuItem.Include(m => m.Category).Include(m => m.SubCategory).ToListAsync(),
                 Categories = await _db.Category.ToListAsync(),
-                Coupons = await _db.Coupon.Where(c=>c.IsActivated==true).ToListAsync()
+                Coupons = await _db.Coupon.Where(c => c.IsActivated == true).ToListAsync()
             };
             return View(IndexVM);
         }
 
+        //GET - Details
         [Authorize]
         public async Task<IActionResult> Details(int id)
         {
@@ -47,6 +50,57 @@ namespace Spice.Areas.Customer.Controllers
             return View(shoppingCart);
 
         }
+
+        //POST - Details
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Details(ShoppingCart cartObject)
+        {
+            cartObject.Id = 0;
+
+            if (ModelState.IsValid)
+            {
+                var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+                var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+                cartObject.ApplicationUserId = claim.Value;
+
+                ShoppingCart cartFromDb = await _db.ShoppingCart.Where(c =>
+                        c.ApplicationUserId == cartObject.ApplicationUserId && c.MenuItemId == cartObject.MenuItemId)
+                    .FirstOrDefaultAsync();
+
+                if (cartFromDb == null)
+                {
+                    await _db.ShoppingCart.AddAsync(cartObject);
+                }
+                else
+                {
+                    cartFromDb.Count += cartObject.Count;
+                }
+
+                await _db.SaveChangesAsync();
+                var count = _db.ShoppingCart.Where(c => c.ApplicationUserId == cartObject.ApplicationUserId).ToList()
+                    .Count;
+
+                HttpContext.Session.SetInt32("ssCartCount", count);
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                var menuItemFromDb = await _db.MenuItem.Include(m => m.Category).Include(m => m.SubCategory)
+                    .Where(m => m.Id == cartObject.MenuItemId).FirstOrDefaultAsync();
+                ShoppingCart shoppingCart = new ShoppingCart()
+                {
+                    MenuItem = menuItemFromDb,
+                    MenuItemId = menuItemFromDb.Id
+                };
+
+                return View(cartObject);
+            }
+
+        }
+
 
         public IActionResult Privacy()
         {
