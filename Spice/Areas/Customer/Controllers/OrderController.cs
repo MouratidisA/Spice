84f5546861303ec2cf6a04eb98b351b89f5b528b
Mouratidis Anargyros <argiris95@windowslive.com>
2019-10-02 15:02:41 +0300

    Paging Method inside Order Controller

--- Spice/Areas/Customer/Controllers/OrderController.cs
+++ Spice/Areas/Customer/Controllers/OrderController.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Stripe;
 
 namespace Spice.Areas.Customer.Controllers
 {
@@ -15,22 +16,23 @@ namespace Spice.Areas.Customer.Controllers
     public class OrderController : Controller
     {
         private readonly ApplicationDbContext _db;
+        private int PageSize = 2;
 
         public OrderController(ApplicationDbContext db)
         {
-            _db = db;            
+            _db = db;
         }
 
         [Authorize]
         public async Task<IActionResult> Confirm(int id)
         {
-            var claimsIdentity = (ClaimsIdentity) this.User.Identity;
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
             var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
 
             OrderDetailsViewModel orderDetailsViewModel = new OrderDetailsViewModel()
             {
-                OrderHeader= await _db.OrderHeader.Include(h=>h.ApplicationUser).FirstOrDefaultAsync(h=>h.Id==id && h.UserId == claim.Value),
-                OrderDetails = await _db.OrderDetails.Where(d=>d.OrderId == id).ToListAsync()
+                OrderHeader = await _db.OrderHeader.Include(h => h.ApplicationUser).FirstOrDefaultAsync(h => h.Id == id && h.UserId == claim.Value),
+                OrderDetails = await _db.OrderDetails.Where(d => d.OrderId == id).ToListAsync()
             };
 
             return View(orderDetailsViewModel);
@@ -45,12 +47,17 @@ namespace Spice.Areas.Customer.Controllers
 
 
         [Authorize]
-        public async Task<IActionResult> OrderHistory(int cartId)
+        public async Task<IActionResult> OrderHistory(int productPage = 1)
         {
             var claimsIdentity = (ClaimsIdentity)this.User.Identity;
             var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
 
-            List<OrderDetailsViewModel> orderList = new List<OrderDetailsViewModel>();
+            OrderListViewModel orderListViewModel = new OrderListViewModel()
+            {
+                Orders = new List<OrderDetailsViewModel>()
+            };
+
+
             List<OrderHeader> orderHeaderList = await _db.OrderHeader.Include(h => h.ApplicationUser).Where(h => h.UserId == claim.Value).ToListAsync();
 
             foreach (OrderHeader item in orderHeaderList)
@@ -60,10 +67,21 @@ namespace Spice.Areas.Customer.Controllers
                     OrderHeader = item,
                     OrderDetails = await _db.OrderDetails.Where(d => d.OrderId == item.Id).ToListAsync()
                 };
-                orderList.Add(individual);
+                orderListViewModel.Orders.Add(individual);
             }
 
-            return View(orderList);
+            var count = orderListViewModel.Orders.Count;
+            orderListViewModel.Orders = orderListViewModel.Orders.OrderByDescending(p => p.OrderHeader.Id).Skip((productPage - 1) * PageSize).Take(PageSize).ToList();
+
+            orderListViewModel.PagingInfo = new PagingInfo()
+            {
+                CurrentPage = productPage,
+                ItemsPerPage = PageSize,
+                TotalItems = count,
+                urlParam = "/Customer/Order/OrderHistory?productPage=:"
+            };
+
+            return View(orderListViewModel);
         }
 
         public async Task<IActionResult> GetOrderDetails(int id)
