78fcc7ec5f2a8169da777f6519ed31a537f100a1
Mouratidis Anargyros <argiris95@windowslive.com>
2019-10-03 14:38:39 +0300

    Order Pickup functionality added

--- Spice/Areas/Customer/Controllers/OrderController.cs
+++ Spice/Areas/Customer/Controllers/OrderController.cs
@@ -254,6 +254,15 @@ namespace Spice.Areas.Customer.Controllers
             return View(orderListVM);
         }
 
-
+        [Authorize(Roles = SD.FrontDeskUser + "," + SD.ManagerUser)]
+        [HttpPost]
+        [ActionName("OrderPickup")]
+        public async Task<IActionResult> OrderPickupPost(int orderId )
+        {
+            OrderHeader orderHeader = await _db.OrderHeader.FindAsync(orderId);
+            orderHeader.Status = SD.StatusCompleted;
+            await _db.SaveChangesAsync();
+            return RedirectToAction("OrderPickup", "Order");
+        }
     }
-}
+    }
